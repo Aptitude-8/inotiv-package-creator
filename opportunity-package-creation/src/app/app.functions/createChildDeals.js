593ff318618e9d18d1fad72be71e1437ee5dc39e
Matt Pickle <mattpickle@mattpickle.net>
2025-11-03 10:57:30 -0600

    Clone all contact and company associations

--- opportunity-package-creation/src/app/app.functions/createChildDeals.js
+++ opportunity-package-creation/src/app/app.functions/createChildDeals.js
@@ -8,7 +8,7 @@ exports.main = async (context = {}, sendResponse) => {
   console.log("Selected studies:", parameters.selectedStudies);
 
   try {
-    // Fetch the deal's contact and companyassociations
+    // Fetch the deal's contact and company associations
     const objectId = propertiesToSend.hs_object_id;
     const url = `https://api.hubapi.com/crm/v3/objects/0-3/${objectId}?associations=0-1,0-2`;
 
@@ -40,19 +40,9 @@ exports.main = async (context = {}, sendResponse) => {
       JSON.stringify(dealWithAssociations, null, 2)
     );
 
-    // Add defensive checks for associations
-    const companies =
-      dealWithAssociations.associations?.companies?.results || [];
-    const contacts = dealWithAssociations.associations?.contacts?.results || [];
-
     const associatedData = {
-      // Get unique company IDs (using Set to remove duplicates)
-      companyIds: [...new Set(companies.map((company) => company.id))],
-
-      // Get primary contact ID (filter for type "primary_contact")
-      primaryContactId: contacts.find(
-        (contact) => contact.type === "primary_contact"
-      )?.id,
+      companyAssociations: dealWithAssociations.associations?.companies?.results || [],
+      contactAssociations: dealWithAssociations.associations?.contacts?.results || [],
 
       // Include the selected studies data
       selectedStudies: parameters.selectedStudies,
@@ -85,18 +75,29 @@ function prepareDealPayloads(associatedData) {
   return associatedData.selectedStudies.map((study) => {
     const baseAssociations = [];
 
-    // Only add primary contact association if we have a contact ID
-    if (associatedData.primaryContactId) {
+    // Add contact associations
+    associatedData.contactAssociations.forEach((assoc) => {
+      const types = assoc.associationTypes.map((type) => ({
+        associationCategory: type.category,
+        associationTypeId: type.typeId,
+      }));
       baseAssociations.push({
-        to: { id: associatedData.primaryContactId },
-        types: [
-          {
-            associationCategory: "USER_DEFINED",
-            associationTypeId: process.env.PRIMARY_CONTACT_ASSOC_ID,
-          },
-        ],
+        to: { id: assoc.id },
+        types: types,
       });
-    }
+    });
+
+    // Add company associations
+    associatedData.companyAssociations.forEach((assoc) => {
+      const types = assoc.associationTypes.map((type) => ({
+        associationCategory: type.category,
+        associationTypeId: type.typeId,
+      }));
+      baseAssociations.push({
+        to: { id: assoc.id },
+        types: types,
+      });
+    });
 
     // Add parent deal association
     baseAssociations.push({
@@ -108,17 +109,6 @@ function prepareDealPayloads(associatedData) {
         },
       ],
     });
-
-    // Add company associations for all company IDs
-    const companyAssociations = associatedData.companyIds.map((companyId) => ({
-      to: { id: companyId },
-      types: [
-        {
-          associationCategory: "HUBSPOT_DEFINED",
-          associationTypeId: process.env.DEAL_TO_COMPANY_ASSOC_ID,
-        },
-      ],
-    }));
 
     // Create the combined dealname
     const parentDealName = associatedData.dealProperties.dealname || "";
@@ -142,7 +132,7 @@ function prepareDealPayloads(associatedData) {
         ...associatedData.generatedValues,
         dealname: combinedDealName, // Override the dealname with our combined version
       },
-      associations: [...baseAssociations, ...companyAssociations],
+      associations: baseAssociations,
     };
   });
 }
